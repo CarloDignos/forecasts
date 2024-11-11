b724dd3263e576447ae3d07aa014a0deceb18f68
Carlo0210 <carlobaluyotdignos1997@gmail.com>
2024-11-11 13:39:52 +0800

    Year Adjustment

--- clients/src/pages/ForecastMonitor.js
+++ clients/src/pages/ForecastMonitor.js
@@ -175,7 +175,7 @@ const EggProductionForecast = () => {
   
 
   const currentYear = new Date().getFullYear();
-  const years = Array.from({ length: 5 }, (_, i) => currentYear - i);
+  const years = Array.from({ length: 6 }, (_, i) => currentYear - i);
 
   const uniqueBreeding = Array.from(new Set(breeding.map((chicken) => chicken.selectedChicken)));
 
